2d9970bc47f574ec5e24a66921a4daf62f21d7c6
Pedro Lopes <lopes.pedro@aluno.ufca.edu.br>
2026-05-13 12:23:26 -0300

    completing exercise 1

--- exercise1/script.js
+++ exercise1/script.js
@@ -12,4 +12,30 @@ function salvarUsuario(event){
 
     bancoDeDados.push(dadosForm);
     console.log(bancoDeDados);
+    mostrarUsuarios();
+}
+
+function mostrarUsuarios() {
+    let lista = document.getElementById("lista");
+    lista.innerHTML = "";
+    for (let i = 0; i < bancoDeDados.length; i++) {
+    lista.innerHTML += `
+      <div class="usuario">
+        <span>
+          ${bancoDeDados[i].usuario}
+        </span>
+                <span>
+          ${bancoDeDados[i].email}
+        </span>
+        <button onclick="remover(${i})">
+          Excluir
+        </button>
+      </div>
+    `;
+  }
+}
+
+function remover(posicao) {
+  bancoDeDados.splice(posicao, 1);
+  mostrarUsuarios();
 }
